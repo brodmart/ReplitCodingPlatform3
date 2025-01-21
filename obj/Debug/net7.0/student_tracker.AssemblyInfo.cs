437543279535965dc2af118761d4305e83c5af48
codingmountains <38632884-codingmountains@users.noreply.replit.com>
2025-01-21 21:41:11 +0000

    Agent query: Is the C# code compiling successfully and faster now?
    Update assembly informational version and add EnableComHosting and EnableGeneratedComInterfaceComImportInterop to MSBuild editorconfig.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/6ecabbee-0c91-419a-b938-5d7c5dcbd1f1/dbe9453c-8fae-4b83-b7a7-b554c9d2b580.jpg

--- obj/Debug/net7.0/student_tracker.AssemblyInfo.cs
+++ obj/Debug/net7.0/student_tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student_tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69d8c497efb915331e3be535b051543c4ea801d")]
 [assembly: System.Reflection.AssemblyProductAttribute("student_tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student_tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
