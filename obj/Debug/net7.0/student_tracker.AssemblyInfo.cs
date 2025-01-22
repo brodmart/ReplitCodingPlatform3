6c4542bb03ba4b034db0ae4f612161dfbc0c7c6d
codingmountains <38632884-codingmountains@users.noreply.replit.com>
2025-01-22 01:20:52 +0000

    Agent query: I understand that the codes aren't working in the web editor. Before we try more code examples, can you confirm if you can see the CodeMirror editor on your screen where you can type code? This will help me provide the right solution.
    Add C++ test workflow and update student attendance tracking program.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/6ecabbee-0c91-419a-b938-5d7c5dcbd1f1/150e4cb0-5d6c-425f-808f-50a6b9f21bfa.jpg

--- obj/Debug/net7.0/student_tracker.AssemblyInfo.cs
+++ obj/Debug/net7.0/student_tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student_tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3286d746f7d921b461542fe45d8f1f412d39d1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("student_tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student_tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- test_input.cs
+++ test_input.cs
@@ -1,16 +1,58 @@
 using System;
+using System.Collections.Generic;
 
 class Program {
     static void Main() {
-        string name;
-        int age;
-        
-        Console.Write("Enter your name: ");
-        name = Console.ReadLine();
-        
-        Console.Write("Enter your age: ");
-        age = int.Parse(Console.ReadLine());
-        
-        Console.WriteLine($"Hello, {name}! You are {age} years old.");
+        var students = new Dictionary<string, int>();
+
+        while (true) {
+            Console.WriteLine("\nStudent Attendance System");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. Mark Attendance");
+            Console.WriteLine("3. View Attendance");
+            Console.WriteLine("4. Exit");
+
+            Console.Write("\nSelect an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice) {
+                case "1":
+                    Console.Write("Enter student name: ");
+                    string name = Console.ReadLine();
+                    if (!students.ContainsKey(name)) {
+                        students[name] = 0;
+                        Console.WriteLine($"Added student: {name}");
+                    } else {
+                        Console.WriteLine("Student already exists!");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Enter student name: ");
+                    name = Console.ReadLine();
+                    if (students.ContainsKey(name)) {
+                        students[name]++;
+                        Console.WriteLine($"Marked attendance for {name}");
+                    } else {
+                        Console.WriteLine("Student not found!");
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine("\nAttendance Records:");
+                    foreach (var student in students) {
+                        Console.WriteLine($"{student.Key}: {student.Value} days");
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("Goodbye!");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+        }
     }
 }
